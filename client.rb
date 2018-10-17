98850fc53d26ff308fd4c4f067626803f164c2f5
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-17 10:40:54 +0100

    Commit with issue

--- client.rb
+++ client.rb
@@ -1,11 +1,14 @@
 require 'socket'
+  server_address = ARGV[0]
+  socket = TCPSocket.new(server_address, 2345)
+  puts socket
 
-  socket = TCPSocket.new('localhost', 2345)
 
 
 puts socket.gets
 puts "Wow what a rude server... I'm going to say Sarah"
   loop do
+    puts server_address
     name = gets.chomp
     socket.puts name
     puts socket.gets
