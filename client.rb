3504568eda04492873c2da1e25d43c8f9dc948ef
Todd Langford-Archer <toddpla@gmail.com>
2018-10-17 10:20:04 +0100

    contiuous server

--- client.rb
+++ client.rb
@@ -1,10 +1,15 @@
 require 'socket'
 
-socket = TCPSocket.new('localhost', 2345)
+  socket = TCPSocket.new('localhost', 2345)
+
 
 puts socket.gets
 puts "Wow what a rude server... I'm going to say Sarah"
-socket.puts "Sarah"
-puts socket.gets
+  loop do
+    name = gets.chomp
+    socket.puts name
+    puts socket.gets
+    exit if name == 'exit'
+  end
 
 socket.close
--- server.rb
+++ server.rb
@@ -5,7 +5,10 @@ server = TCPServer.new(2345)
 loop do
   socket = server.accept
   socket.puts "HELLO CLIENT WHAT IS YOUR NAME?"
-  name = socket.gets.chomp
-  socket.puts "THAT'S A LOVELY NAME #{name}"
+  loop do
+    name = socket.gets.chomp
+    break if name == 'exit'
+    socket.puts "THAT'S A LOVELY NAME #{name}"
+  end
   socket.close
 end
